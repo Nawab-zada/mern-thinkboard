aa9d62f109c5c16cfa97de037a868297012d0f6f
nawab zada <zada86829@gmail.com>
2025-09-20 23:15:46 +0500

    updated

--- backened/server.js
+++ backened/server.js
@@ -31,7 +31,7 @@ app.get("*", (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`✅ Server is running on http://localhost:${PORT}`);
 });
