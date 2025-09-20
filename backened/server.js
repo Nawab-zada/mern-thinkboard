c48d40573fce82b0ef40c4e0ed334d267f54bd99
nawab zada <zada86829@gmail.com>
2025-09-20 23:06:13 +0500

    updated

--- backened/server.js
+++ backened/server.js
@@ -24,14 +24,14 @@ app.use((req,res,next)=>{
 connectDB();
 
 // mongodb+srv://khan234:<db_password>@cluster0.frhqyuy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
-app.use(express.static(path.resolve("frontend", "dist")));
+app.use(express.static(path.resolve("frontend", "vite-project", "dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve("frontend", "dist", "index.html"));
+  res.sendFile(path.resolve("frontend", "vite-project", "dist", "index.html"));
 });
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`✅ Server is running on http://localhost:${PORT}`);
 });
